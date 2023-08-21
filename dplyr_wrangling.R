c09015dfbb6e4f1b1edb10d711d17de01b4385a9
mariamkg00 <mariamkhadijah00@gmail.com>
2023-08-21 15:01:52 -0700

    interactive session

--- dplyr_wrangling.R
+++ dplyr_wrangling.R
@@ -58,5 +58,65 @@ exclude_bb_cc_pie <- pie_crab %>% filter(!site %in% c("BB","CC","PIE")) # is som
 big_sized_pie_crab_nib_cc_zi <- pie_crab %>% filter(site %in% c("NIB","CC","ZI"),
                                                     size > 13)
 
-#-------------------------------Selecting columns---------------------#
+#-------------------------------Selecting columns-----------------------------#
+# we can select ranges of columns at a time, to select individual columns by name, seperaate them by a comma.
 
+crabs_subset <- pie_crab %>% select(latitude, size, water_temp)
+
+#Select a range of columns using :
+crabs_subset2 <- pie_crab %>% select(site:air_temp)
+
+
+# select a range and an individual column
+crab_subset3 <- pie_crab %>% select(date:water_temp, name)
+
+#----------------------------Mutate!---------------------------------#
+
+#Use dplyr::mutate() to add or update a column, while keeping all existing columns
+
+crabs_cm <- pie_crab %>% 
+  mutate(size_cm = size / 10)
+
+#What happens if I use muttae to add a new column containing the mean of the size column
+
+mean_size <- pie_crab %>% 
+  mutate(mean_size= mean(size, na.rm=TRUE))
+
+crabs_awesome <- pie_crab %>% 
+  mutate(name = "Teddy is awesome")
+
+#when in doubt, make a new column, never overwrite columns
+
+# Reminder: groub_by + summarize
+mean_size_by_site <- pie_crab %>% 
+  group_by(site) %>% 
+  summarize(mean_size = mean(size, na.rm = TRUE),
+            sd_size = sd(size, na.rm = TRUE))
+
+#What about a group by then mutate? will be group by site 
+group_mutate <- pie_crab %>% 
+  group_by(site) %>% 
+  mutate(mean_size = mean(size, na.rm = TRUE))
+
+
+
+penguins %>% 
+  group_by(species, island, year) %>% 
+  summarize(mean_body_mass = mean(body_mass_g , na.rm = TRUE))
+
+#What if i want to make a new column in pie_Crab that contains "giant" if the size is greater than 20, not giant if the size is less than or equal to 20
+
+#use dplyr::case_when() to write if-else stataments more easily
+crabs_bin <- pie_crab %>% 
+  mutate(size_binned = case_when(
+    size > 20 ~ "giant ",
+    size <= 20 ~ "not giant"
+  ))
+
+
+sites_bin <- pie_crab %>% 
+  mutate(region = case_when(
+    site %in% c("ZI","CC","PIE") ~ "LOW",
+    site %in% c("BB","NIB") ~ "MIDDLE",
+    .default = "HIGH"
+  ))
